a9f8b600a902a6fc58254bbb4f8bba0d0fecc70f
MichaelWang <michael_wangguanyi@163.com>
2025-02-07 23:10:48 +0800

    initialize_candidate && vote

--- programs/voting/src/lib.rs
+++ programs/voting/src/lib.rs
@@ -21,6 +21,71 @@ pub mod voting {
         poll.candidate_amount = 0;
         Ok(())
     }
+
+    pub fn initialize_candidate(
+        ctx: Context<InitializeCandidate>,
+        _poll_id: u64,
+        candidate_name: String,
+    ) -> Result<()> {
+        let candidate = &mut ctx.accounts.candidate;
+        candidate.name = candidate_name;
+        candidate.votes = 0;
+
+        let poll = &mut ctx.accounts.poll;
+        poll.candidate_amount += 1;
+
+        Ok(())
+    }
+
+    pub fn vote(ctx: Context<Vote>, _poll_id: u64, _candidate_name: String) -> Result<()> {
+        let candidate = &mut ctx.accounts.candidate;
+        candidate.votes += 1;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(poll_id: u64, candidate_name: String)]
+pub struct Vote<'info> {
+    #[account(
+        mut,
+        seeds=[poll_id.to_le_bytes().as_ref(), candidate_name.as_ref()],
+        bump
+    )]
+    pub candidate: Account<'info, Candidate>,
+}
+
+#[derive(Accounts)]
+#[instruction(poll_id: u64, candidate_name:String)]
+pub struct InitializeCandidate<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [poll_id.to_le_bytes().as_ref()],
+        bump
+    )]
+    pub poll: Account<'info, Poll>,
+
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + Candidate::INIT_SPACE,
+        seeds = [poll_id.to_le_bytes().as_ref(), candidate_name.as_ref()],
+        bump
+    )]
+    pub candidate: Account<'info, Candidate>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Candidate {
+    #[max_len(32)]
+    name: String,
+    votes: u64,
 }
 
 #[derive(Accounts)]
--- tests/voting.ts
+++ tests/voting.ts
@@ -1,4 +1,3 @@
-import * as anchor from "@coral-xyz/anchor";
 import { Program, BN } from "@coral-xyz/anchor";
 import { Voting } from "../target/types/voting";
 import { BankrunProvider, startAnchor } from 'anchor-bankrun';
@@ -8,16 +7,24 @@ const IDL = require('../target/idl/voting.json');
 const votingAddress = new PublicKey('18maLjJs5ZXfmeAzSgq5mNyNK5QoZrbUYUMFW6CC3XR');
 
 describe("voting", () => {
+  let context;
+  let provider;
+  let votingProgram: Program<Voting>;
+  let candidate1Address;
+  let candidate2Address;
+  let pollAccountAddress;
 
-  it("Initialize Poll ", async () => {
-    const context = await startAnchor("", [{ name: 'Voting', programId: votingAddress }], []);
-    const provider = new BankrunProvider(context);
+  before(async () => {
+    context = await startAnchor("", [{ name: 'Voting', programId: votingAddress }], []);
+    provider = new BankrunProvider(context);
 
-    const votingProgram = new Program<Voting>(
+    votingProgram = new Program<Voting>(
       IDL,
       provider,
     );
+  })
 
+  it("Initialize Poll", async () => {
     const pollId = new BN(1);
     const start = new BN(0);
     const end = new BN(1800000000);
@@ -29,7 +36,7 @@ describe("voting", () => {
       " What's your favorite fruit?"
     ).rpc();
 
-    const [pollAccountAddress] = PublicKey.findProgramAddressSync(
+    [pollAccountAddress] = PublicKey.findProgramAddressSync(
       // seeds
       [pollId.toArrayLike(Buffer, 'le', 8)],
       votingProgram.programId
@@ -41,6 +48,78 @@ describe("voting", () => {
     expect(poll.pollStart.eq(start));
     expect(poll.pollEnd.eq(end));
     expect(poll.pollStart.toNumber()).lessThan(poll.pollEnd.toNumber());
-    
   });
+
+
+  it("initialize candidate", async () => {
+    const pollId = new BN(1);
+    await votingProgram.methods.initializeCandidate(
+      pollId,
+      "apple"
+    ).rpc();
+
+    [candidate1Address] = PublicKey.findProgramAddressSync(
+      [pollId.toArrayLike(Buffer, 'le', 8), Buffer.from("apple")],
+      votingProgram.programId,
+    );
+
+    const candidate1 = await votingProgram.account.candidate.fetch(candidate1Address);
+    expect(candidate1.name).eq("apple");
+    expect(candidate1.votes.toNumber()).eq(0);
+
+    let poll = await votingProgram.account.poll.fetch(pollAccountAddress);
+    expect(poll.candidateAmount.toNumber()).eq(1);
+
+    await votingProgram.methods.initializeCandidate(
+      pollId,
+      "banana"
+    ).rpc();
+
+    [candidate2Address] = PublicKey.findProgramAddressSync(
+      [pollId.toArrayLike(Buffer, 'le', 8), Buffer.from("banana")],
+      votingProgram.programId,
+    );
+
+    const candidate2 = await votingProgram.account.candidate.fetch(candidate2Address);
+    expect(candidate2.name).eq("banana");
+    expect(candidate2.votes.toNumber()).eq(0);
+
+    poll = await votingProgram.account.poll.fetch(pollAccountAddress);
+    expect(poll.candidateAmount.toNumber()).eq(2);
+  })
+
+  it("vote ", async () => {
+    const pollId = new BN(1);
+    await votingProgram.methods.vote(
+      pollId,
+      "apple"
+    ).rpc();
+
+    let candidate = await votingProgram.account.candidate.fetch(candidate1Address);
+    expect(candidate.votes.toNumber()).eq(1);
+
+    await votingProgram.methods.vote(
+      pollId,
+      "apple"
+    ).rpc();
+
+    candidate = await votingProgram.account.candidate.fetch(candidate1Address);
+    expect(candidate.votes.toNumber()).eq(2);
+
+    await votingProgram.methods.vote(
+      pollId,
+      "banana"
+    ).rpc();
+
+    candidate = await votingProgram.account.candidate.fetch(candidate2Address);
+    expect(candidate.votes.toNumber()).eq(1);
+
+    await votingProgram.methods.vote(
+      pollId,
+      "banana"
+    ).rpc();
+
+    candidate = await votingProgram.account.candidate.fetch(candidate2Address);
+    expect(candidate.votes.toNumber()).eq(2);
+  })
 });
